f0edf738bcc4c0cb26e5251f6f864b07fe5d0f17
TINY-KE <zhangjiadong@buaa.edu.cn>
2024-09-10 14:53:12 +0800

    新kinect指向物体

--- README.md
+++ README.md
@@ -3,13 +3,29 @@
 
 ## 各Demo的作用
 
-1. run_gazebo.launch  运行gazebo仿真环境
+### run_gazebo.launch  运行gazebo仿真环境
+roslaunch  gazebo_object_env_2024 run_gazebo.launch 
 
-2. move_base_config.launch   运行movebase
+### 直接控制相机的位姿
+   1. 环形
+    rosrun gazebo_object_env_2024 circle
+   2. 移动物体，来改变相机
+    rosrun gazebo_object_env_2024 object_direction
+    
+## Active QSP SLAM 流程
+   1. QSP SLAM：基于二次曲面和隐式形状表达的物体级 SLAM
+      在空间中建立二次曲面和点云之间的关联关系，为隐式形状编码提供更准确的初始位姿和更完整的点云信息。	
+   2. 基于
 
-3. navigation.launch  综合型launch文件
+
+<!-- 2. move_base_config.launch   运行movebase -->
+
+<!-- 3. navigation.launch  综合型launch文件
    1. 构建栅格地图
    2. 发布map和odom的tf
    3. 发布机器人和odom的tf
    4. 运行move_base_config.launch
-   
+    -->
+
+
+
--- src/circle.cpp
+++ src/circle.cpp
@@ -14,6 +14,39 @@
 #include <eigen3/Eigen/Core>
 #include <eigen3/Eigen/Dense>
 
+
+void setPose(ros::ServiceClient& set_client, std::string name, double x, double y, double z, tfScalar qw, tfScalar qx, tfScalar qy, tfScalar qz)
+{
+    gazebo_msgs::SetModelState object_state;
+    
+    object_state.request.model_state.model_name = name;//"acircles_pattern_0"  mobile_base;
+
+    
+    object_state.request.model_state.pose.position.x = x;
+    object_state.request.model_state.pose.position.y = y;
+    object_state.request.model_state.pose.position.z = z;
+
+    object_state.request.model_state.pose.orientation.w = qw;
+    object_state.request.model_state.pose.orientation.x = qx;
+    object_state.request.model_state.pose.orientation.y = qy;
+    object_state.request.model_state.pose.orientation.z = qz;
+    
+    object_state.request.model_state.twist.linear.x = 0.0;
+    object_state.request.model_state.twist.linear.y = 0.0;
+    object_state.request.model_state.twist.linear.z = 0.0;
+    object_state.request.model_state.twist.angular.x = 0.0;
+    object_state.request.model_state.twist.angular.y = 0.0;
+    object_state.request.model_state.twist.angular.z = 0.0;
+
+    object_state.request.model_state.reference_frame = "world";
+
+    if(set_client.call(object_state))
+        ROS_INFO("Success '%s' call service", name.c_str());
+    else
+        ROS_INFO("Error '%s' call service",name.c_str());
+}
+
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "gazebo_set_states_publisher");
@@ -29,21 +62,18 @@ int main(int argc, char **argv)
     double angle = 0;
     double center_x = distance;
     double center_y = 0;
-    objstate.request.model_state.model_name = "mobile_base";//"acircles_pattern_0"  mobile_base;
-    objstate.request.model_state.pose.position.x = distance*cos(angle) - center_x;
-    objstate.request.model_state.pose.position.y = distance*sin(angle) - center_y;
-    objstate.request.model_state.pose.position.z = height;
+    std::string camera_name = "kinect";
+    double camera_x = distance*cos(angle) - center_x;
+    double camera_y = distance*sin(angle) - center_y;
+    double camera_z = height;
     tf::Quaternion q_init = tf::createQuaternionFromRPY(0, pitch*M_PI/180.0, angle+M_PI);
     objstate.request.model_state.pose.orientation.w = q_init.w();
     objstate.request.model_state.pose.orientation.x = q_init.x();
     objstate.request.model_state.pose.orientation.y = q_init.y();
     objstate.request.model_state.pose.orientation.z = q_init.z();
-    objstate.request.model_state.twist.linear.x = 0.0;
-    objstate.request.model_state.twist.linear.y = 0.0;
-    objstate.request.model_state.twist.linear.z = 0.0;
-    objstate.request.model_state.twist.angular.x = 0.0;
-    objstate.request.model_state.twist.angular.y = 0.0;
-    objstate.request.model_state.twist.angular.z = 0.0;
+
+    setPose(client, camera_name, camera_x, camera_y, camera_z, q_init.w(), q_init.x(), q_init.y(), q_init.z());
+
     objstate.request.model_state.reference_frame = "world";
     std::cout<<"camerabody pose x:"<<objstate.request.model_state.pose.position.x
                 <<",y:"<<objstate.request.model_state.pose.position.y
@@ -85,35 +115,25 @@ int main(int argc, char **argv)
         double y = distance*sin(angle);
         double z = height;
         double mean_x=0, mean_y=0;
-        //cv::Mat view = (cv::Mat_<float>(3, 1) << mean_x-x, mean_y-y, 0);
-        //double angle = atan( (mean_y-y)/(mean_x-x) );
-        //if( (mean_x-x)<0 && (mean_y-y)>0 )
-        //    angle = angle +  M_PI;
-        //if( (mean_x-x)<0 && (mean_y-y)<0 )
-        //    angle = angle -  M_PI;
-        //// Eigen::AngleAxisd rotation_vector (angle, Eigen::Vector3d(0,0,1));
-        //// Eigen::Matrix3d rotation_matrix = rotation_vector.toRotationMatrix();
-        //// cv::Mat rotate_mat = Converter::toCvMat(rotation_matrix);
+
         tf::Quaternion q = tf::createQuaternionFromRPY(0, pitch/180.0*M_PI, angle+M_PI);
 
-        objstate.request.model_state.pose.position.x = x - center_x;
-        objstate.request.model_state.pose.position.y = y - center_y;
-        objstate.request.model_state.pose.position.z = z;
-        objstate.request.model_state.pose.orientation.w = q.w();
-        objstate.request.model_state.pose.orientation.x = q.x();
-        objstate.request.model_state.pose.orientation.y = q.y();
-        objstate.request.model_state.pose.orientation.z = q.z();
+        camera_x = x - center_x;
+        camera_y = y - center_y;
+        camera_z = z;
 
-        std::cout<<"pose x:"<<objstate.request.model_state.pose.position.x
-                <<",y:"<<objstate.request.model_state.pose.position.y
-                <<",z:"<<objstate.request.model_state.pose.position.z
+
+        std::cout<<"pose x:"<<camera_x
+                <<",y:"<<camera_y
+                <<",z:"<<camera_z
                 <<",qw:"<<q.w()
                 <<",qx:"<<q.x()
                 <<",qy:"<<q.y()
                 <<",qz:"<<q.z()
                 <<std::endl;
-        client.call(objstate);
-        ROS_INFO("call service");
+        
+        setPose(client, camera_name, camera_x, camera_y, camera_z, q.w(), q.x(), q.y(), q.z());
+    
 
         cout += 0.01;
         ros::spinOnce();
